822241bc5af34ab435da61f11b86fa6af23ba3d2
BeLieVer031 <sandyrajak031@gmail.com>
2025-02-26 17:42:02 +0530

    Update root layout

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,15 +1,15 @@
 import { useUser } from "@clerk/clerk-react"
-import React, { useEffect } from "react"
-import { Outlet, useNavigate } from "react-router-dom"
+import React from "react"
+import { Outlet } from "react-router-dom"
 
 function RootLayout() {
-    const { isSignedIn, isLoaded } = useUser()
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (isLoaded && !isSignedIn) {
-            navigate("/auth", { replace: true });
-        }
-    }, [isLoaded, isSignedIn, navigate]);
+    const { isLoaded } = useUser()
+    // const navigate = useNavigate();
+    // useEffect(() => {
+    //     if (isLoaded && !isSignedIn) {
+    //         navigate("/auth", { replace: true });
+    //     }
+    // }, [isLoaded, isSignedIn, navigate]);
 
     if (!isLoaded) {
         return <div>Loading...</div>
